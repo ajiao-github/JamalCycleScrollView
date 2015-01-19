5c6b900f4d801969bdd1010d5108119c1da4b321
jamalping <silvercreek@Ryans-MBA.local>
2015-01-19 17:14:53 +0800

    Initial Commit

---
+++ JamalCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JamalCycleScrollView
+//
+//  Created by jamalping on 15-1-19.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
